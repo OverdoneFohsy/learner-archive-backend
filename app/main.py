09186fe4fba6362a7f34c50770c29d1f2edb7754
Foh Shawn Young <shawn.foh@gmail.com>
2025-12-08 18:15:45 +0800

    Modify api endpoint import syntax in app/main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.api.transcript import router as transcript_router
-from app.api.chunk import router as chunk_router
+from app.api import transcript, chunk, ingestion
 
 app = FastAPI(title="Ask My Youtuber Backend")
 
-app.include_router(transcript_router, prefix="/api", tags=["Transcript"])
-app.include_router(chunk_router, prefix="/api", tags=["Chunk"])
+app.include_router(transcript.router, prefix="/api", tags=["Transcript"])
+app.include_router(chunk.router, prefix="/api", tags=["Chunk"])
+app.include_router(ingestion.router, prefix="/api", tags=["Ingestion"])
 
 @app.get("/")
 def root():
