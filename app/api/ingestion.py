4d21ddcff919bbb43ec44e6f5db314000bc310dc
Foh Shawn Young <shawn.foh@gmail.com>
2025-12-10 17:36:37 +0800

    Debug IngestionService class and API.

--- app/api/ingestion.py
+++ app/api/ingestion.py
@@ -7,8 +7,8 @@ def process_video_pipeline(
     video_id:str, 
     max_chars: int = 2000, 
     overlap_chars: int = 300, 
-    ingestopm_service: IngestionService = Depends(get_ingestion_service)):
+    ingestion_service: IngestionService = Depends(get_ingestion_service)):
 
-    response = ingestopm_service.process_video(video_id=video_id, max_chars=max_chars, overlap_chars=overlap_chars)
+    response = ingestion_service.process_video(video_id=video_id, max_chars=max_chars, overlap_chars=overlap_chars)
 
     return response
--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -2,6 +2,7 @@ from fastapi import Depends
 from app.services.transcript_service import TranscriptService
 from app.services.chunk import ChunkService
 from app.services.vector_db import VectorDBService
+from app.services import transcript_service, chunk, vector_db
 
 class IngestionService:
     def __init__(self, transcript_service: TranscriptService, chunk_service: ChunkService, vector_db_service:VectorDBService):
@@ -14,13 +15,13 @@ class IngestionService:
 
         chunk_response = self.chunk_service.get_chunks(transcript=transcript_response, max_chars=max_chars, overlap_chars=overlap_chars)
 
-        upload_status = self.vector_db_service.ingest_documents(documents=chunk_response)
+        upload_status = self.vector_db_service.ingest_documents(documents=chunk_response.chunk)
 
         return upload_status
 
 def get_ingestion_service(
-        transcript_service: TranscriptService = Depends(TranscriptService()),
-        chunk_service: ChunkService = Depends(ChunkService()),
-        vector_db_service: VectorDBService = Depends(VectorDBService)
+        transcript_service: TranscriptService = Depends(transcript_service.get_transcript_service),
+        chunk_service: ChunkService = Depends(chunk.get_chunk_service),
+        vector_db_service: VectorDBService = Depends(vector_db.get_vector_db_service)
 ):
     return IngestionService(transcript_service=transcript_service, chunk_service=chunk_service, vector_db_service=vector_db_service)
