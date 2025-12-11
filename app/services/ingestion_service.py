2fb03ccdb83756fa2427f3ecd6a448057e0371e7
Foh Shawn Young <shawn.foh@gmail.com>
2025-12-11 15:57:02 +0800

    Updated chunk_service file name to match other services

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -1,9 +1,9 @@
 from fastapi import Depends
 # from app.services.chunk import ChunkService
-from app.services import transcript_service, chunk, vector_db, embedding_service
+from app.services import chunk_service, transcript_service, vector_db, embedding_service
 
 class IngestionService:
-    def __init__(self, transcript_service: transcript_service.TranscriptService, chunk_service: chunk.ChunkService, embedding_service: embedding_service.EmbeddingService,vector_db_service:vector_db.VectorDBService):
+    def __init__(self, transcript_service: transcript_service.TranscriptService, chunk_service: chunk_service.ChunkService, embedding_service: embedding_service.EmbeddingService,vector_db_service:vector_db.VectorDBService):
         self.transcript_service = transcript_service
         self.chunk_service = chunk_service
         self.embedding_service = embedding_service
@@ -46,7 +46,7 @@ class IngestionService:
 
 def get_ingestion_service(
         transcript_service: transcript_service.TranscriptService = Depends(transcript_service.get_transcript_service),
-        chunk_service: chunk.ChunkService = Depends(chunk.get_chunk_service),
+        chunk_service: chunk_service.ChunkService = Depends(chunk_service.get_chunk_service),
         embedding_service: embedding_service.EmbeddingService = Depends(embedding_service.get_embedding_service),
         vector_db_service: vector_db.VectorDBService = Depends(vector_db.get_vector_db_service)
 ):
