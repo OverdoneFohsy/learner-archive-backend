c6a253eaa1c36d6c79de6957195a8c619b58343b
Foh Shawn Young <shawn.foh@gmail.com>
2025-12-11 15:33:23 +0800

    Complete working ingestion pipeline service and api.

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -1,13 +1,12 @@
 from fastapi import Depends
-from app.services.transcript_service import TranscriptService
-from app.services.chunk import ChunkService
-from app.services.vector_db import VectorDBService
-from app.services import transcript_service, chunk, vector_db
+# from app.services.chunk import ChunkService
+from app.services import transcript_service, chunk, vector_db, embedding_service
 
 class IngestionService:
-    def __init__(self, transcript_service: TranscriptService, chunk_service: ChunkService, vector_db_service:VectorDBService):
+    def __init__(self, transcript_service: transcript_service.TranscriptService, chunk_service: chunk.ChunkService, embedding_service: embedding_service.EmbeddingService,vector_db_service:vector_db.VectorDBService):
         self.transcript_service = transcript_service
         self.chunk_service = chunk_service
+        self.embedding_service = embedding_service
         self.vector_db_service = vector_db_service
 
     def process_video(self, video_id: str, max_chars: int = 2000, overlap_chars: int = 300):
@@ -15,13 +14,40 @@ class IngestionService:
 
         chunk_response = self.chunk_service.get_chunks(transcript=transcript_response, max_chars=max_chars, overlap_chars=overlap_chars)
 
-        upload_status = self.vector_db_service.ingest_documents(documents=chunk_response.chunk)
+        chunks = chunk_response.chunk
+
+        texts_to_embed = [c.text for c in chunks]
+
+        if not chunks:
+            return {"status": "success", "total_count": 0, "message": "No chunks generated."}
+        
+        print(f"Generating embeddings for {len(texts_to_embed)} chunks...")
+
+        vectors = self.embedding_service.embed_texts(texts_to_embed)
+
+        if len(vectors) != len(texts_to_embed):
+            raise RuntimeError(f"Embedding service vector count mismatch. Expected {len(chunks)}, got {len(vectors)}.")
+
+        documents_to_ingest = []
+
+        for chunk_model, vector_data in zip(chunks, vectors):
+
+            chunk_model.vector = vector_data
+
+            documents_to_ingest.append(chunk_model.model_dump(exclude_none=True))
+
+        print(f"Starting ingestion of {len(documents_to_ingest)} documents into Pinecone...")
+
+        upload_status = self.vector_db_service.ingest_documents(documents=documents_to_ingest)
+
+        print(upload_status)
 
         return upload_status
 
 def get_ingestion_service(
-        transcript_service: TranscriptService = Depends(transcript_service.get_transcript_service),
-        chunk_service: ChunkService = Depends(chunk.get_chunk_service),
-        vector_db_service: VectorDBService = Depends(vector_db.get_vector_db_service)
+        transcript_service: transcript_service.TranscriptService = Depends(transcript_service.get_transcript_service),
+        chunk_service: chunk.ChunkService = Depends(chunk.get_chunk_service),
+        embedding_service: embedding_service.EmbeddingService = Depends(embedding_service.get_embedding_service),
+        vector_db_service: vector_db.VectorDBService = Depends(vector_db.get_vector_db_service)
 ):
-    return IngestionService(transcript_service=transcript_service, chunk_service=chunk_service, vector_db_service=vector_db_service)
+    return IngestionService(transcript_service=transcript_service, chunk_service=chunk_service, embedding_service=embedding_service,vector_db_service=vector_db_service)
